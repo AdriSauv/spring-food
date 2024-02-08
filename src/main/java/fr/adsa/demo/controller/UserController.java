fddfcc900e58e042c19f9b196962c086e6f00007
Adrien <adriensauvage59@gmail.com>
2024-02-08 10:20:54 +0100

    ajout methode findbyemail

--- src/main/java/fr/adsa/demo/controller/UserController.java
+++ src/main/java/fr/adsa/demo/controller/UserController.java
@@ -20,4 +20,12 @@ public class UserController {
         User savedUser = userRepository.save(user);
         return savedUser;
     }
+
+    public User findByEmail(String email) throws Exception {
+        User user = userRepository.findByEmail(email);
+        if(user!=null){
+            throw new Exception("user not found with email: "+email);
+        }
+        return user;
+    }
 }
