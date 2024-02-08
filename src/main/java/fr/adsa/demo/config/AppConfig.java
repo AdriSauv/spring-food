d38ba89cdf8c5b0ea5bd99b71aa7cf9fac684c33
Adrien <adriensauvage59@gmail.com>
2024-02-08 13:43:36 +0100

    ajout dependences jwt + ajout spring security

---
+++ src/main/java/fr/adsa/demo/config/AppConfig.java
@@ -0,0 +1,36 @@
+package fr.adsa.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        Authorize -> Authorize.requestMatchers("/api/**").authenticated()
+                                .anyRequest().permitAll())
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            var cors = new org.springframework.web.cors.CorsConfiguration();
+            cors.setAllowedOrigins(java.util.List.of("http://localhost:3306"));
+            cors.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE"));
+            return cors;
+        };
+    }
+}
